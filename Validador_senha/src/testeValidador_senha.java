90ee6733efbfbce178e74c42720439993da10cdb
Vítor Gabriel Campolina Silva <vitor.campolina2013@gmail.com>
2025-05-21 15:05:13 -0300

    Criação da lógica para fazer o teste de validação de senha passar no verde.

--- Validador_senha/src/testeValidador_senha.java
+++ Validador_senha/src/testeValidador_senha.java
@@ -1,16 +1,16 @@
 import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 
 public class testeValidador_senha {
 
 
-	    @Test
-	    public void senhaValida() {
-	        validador_senha validador = new validador_senha();
-	        boolean valido = validador.validar("Senha123!");
-	        assertTrue(valido);
-	    }
-
+    @Test
+    public void senhaValida() {
+        validador_senha validador = new validador_senha();
+        boolean valido = validador.validar("Senha123!");
+        assertTrue(valido);
+        assertTrue(validador.getErros().isEmpty());
+    }
+  
+	    
 }
--- Validador_senha/src/validador_senha.java
+++ Validador_senha/src/validador_senha.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class validador_senha {
-    
+
+    private List<String> erros = new ArrayList<>();
+
     public boolean validar(String senha) {
-        return false;
+        return erros.isEmpty();
+    }
+
+    public List<String> getErros() {
+        return erros;
     }
 }
