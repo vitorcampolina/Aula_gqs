3b5e84537595b77cd663ab3561b020e8ee410904
CamaradaSam <samuel_pdsantos@hotmail.com>
2025-05-22 15:32:50 -0300

    RED: Teste para senha com menos de 8 caracteres

---
+++ Validador_senha/src/ValidadorSenhaComMenosDeOitoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ValidadorSenhaComMenosDeOitoTest {
+
+    @Test
+    public void senhaSemMaiuscula_DeveSerInvalida() {
+        validador_senha validador = new validador_senha();
+        boolean valido = validador.validar("Sen1@");
+        assertFalse(valido);
+        assertTrue(validador.getErros().contains("A senha deve ter pelo menos 8 caracteres"));
+    }
+}
+
+
